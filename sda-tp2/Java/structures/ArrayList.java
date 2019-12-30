c97a28fe7698cd0b2470c602c9341cfa871fe249
LEMinhHao <56441152+LEMinhHao@users.noreply.github.com>
2019-12-30 22:37:45 +0100

    SDA_TP2

---
+++ sda-tp2/Java/structures/ArrayList.java
@@ -0,0 +1,143 @@
+package structures;
+
+import java.util.Arrays;
+
+/**
+ * Cette classe est un proxy pour les ArrayList, c'est à dire les tableaux
+ * dynamiques en Java. On utilise cette classe afin d'avoir le contrôle sur la
+ * gestion de la mémoire.
+ */
+public class ArrayList<T> {
+
+	/**
+	 * Constructeur de la classe des tableaux dynamiques. Complexité en
+	 * temps/espace, pire et meilleur cas : O(1)
+	 */
+	public ArrayList() {
+		this.capacity = 4;
+		this.data = new Object[this.capacity];
+	}
+
+	/**
+	 * Ajoute une valeur dans le tableau. Complexité en temps/espace, pire cas :
+	 * O(size) Complexité en temps/espace, meilleur cas : O(1) Complexité amortie :
+	 * O(1)
+	 * 
+	 * @param x est la valeur que l'on souhaite ajouter.
+	 * @returns true si le tableau a été agrandit, false sinon
+	 */
+	public boolean append(T x) {
+		boolean memory_allocation = do_we_need_to_enlarge_capacity();
+		if (memory_allocation) {
+			enlarge_capacity();
+		}
+		data[size++] = x;
+		return memory_allocation;
+	}
+
+	/**
+	 * Supprime la dernière valeur du tableau. Complexité en temps, pire cas :
+	 * O(size) Complexité en temps, meilleur cas : O(1) Complexité amortie : O(1)
+	 * 
+	 * @returns true si le tableau a été réduit, false sinon
+	 */
+	public boolean pop_back() {
+		boolean memory_reduction = do_we_need_to_reduce_capacity();
+		if (size != 0) {
+			if (memory_reduction)
+				reduce_capacity();
+			size--;
+		}
+		return memory_reduction;
+	}
+
+	/**
+	 * Renvoie la valeur située à la position donnée par l'utilisateur. Complexité
+	 * en temps/espace, pire cas : O(1)
+	 * 
+	 * @param pos est la l'indice de la case on l'utilisateur veut connaître la
+	 *            valeur.
+	 * @returns la valeur située à la position donnée par l'utilisateur.
+	 */
+	@SuppressWarnings("unchecked")
+	T get(int pos) {
+		return (T) data[pos];
+	}
+
+	/**
+	 * Renvoie le nombre d'éléments stockés dans le tableau. Complexité en
+	 * temps/espace, pire cas : O(1)
+	 * 
+	 * @returns le nombre d'éléments stockés dans le tableau.
+	 */
+	public int size() {
+		return size;
+	}
+
+	/**
+	 * Renvoie la capacité de stockage du tableau. Complexité en temps/espace, pire
+	 * cas : O(1)
+	 * 
+	 * @returns le nombre d'éléments stockés dans le tableau.
+	 */
+	public int capacity() {
+		return capacity;
+	}
+
+	/**
+	 * Cette fonction détermine la règle selon laquelle un espace mémoire plus grand
+	 * sera alloué ou non.
+	 * 
+	 * @returns true si le tableau doit être agrandi, false sinon.
+	 */
+	private boolean do_we_need_to_enlarge_capacity() {
+		return size == capacity;
+		// return size >= (capacity * 3) / 4;
+	}
+
+	/**
+	 * Cette fonction augmente la capacité du tableau.
+	 */
+	private void enlarge_capacity() {
+//		capacity *= 2;
+//		data = java.util.Arrays.copyOf(data, capacity);
+		capacity = (int) (capacity + Math.sqrt(capacity));
+		data = java.util.Arrays.copyOf(data, capacity);
+	}
+
+	/**
+	 * Cette fonction détermine la règle selon laquelle un espace mémoire plus petit
+	 * sera alloué ou non.
+	 * 
+	 * @returns true si le tableau doit être réduit, false sinon.
+	 */
+	private boolean do_we_need_to_reduce_capacity() {
+//		 return size <= capacity / 4 && size > 4;
+		int sqrt = (int) Math.sqrt(capacity);
+		return size <= capacity - 2 * sqrt && size > 4;
+	}
+
+	/**
+	 * Cette fonction reduit la capacité du tableau.
+	 */
+	void reduce_capacity() {
+//		capacity = capacity * 2 / 3;
+//		data = java.util.Arrays.copyOf(data, capacity);
+		int new_capacity = (int) (capacity - Math.sqrt(capacity));
+		if (new_capacity < 4) {
+			capacity = 4;
+		} else {
+			capacity = (int) (capacity - Math.sqrt(capacity));
+		}
+		data = java.util.Arrays.copyOf(data, capacity);
+
+	}
+
+	// Tableau dynamique en Java. Sa capacité réelle est masquée, mais on peut avoir
+	// un contrôle dessus.
+	private Object[] data;
+	// Capacité réelle du tableau data.
+	private int capacity;
+	// Nombre réel d'éléments dans le tableau.
+	private int size;
+}
